feadf4bb1ff68c53832866b218182315fd94fad9
Amit Kumar <akumar27@sapient.com>
2016-11-02 09:06:16 -0400

    Feature added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var exphbs  = require('express-handlebars');
 var path = require('path');
 var fs = require('fs');
 var url = require('url');
+var jsonQuery = require('json-query')
 
 var app = express();
 
@@ -13,7 +14,6 @@ var hbs = exphbs.create({
 	layoutsDir : __dirname + '/views/layouts',
 	partialsDir : __dirname + '/views'	
 });
-
 
 app.engine('hbs',hbs.engine);
 app.set('view engine', 'hbs');
@@ -40,7 +40,11 @@ app.get('/feature', function (req, res) {
 	var queryData = url.parse(req.url, true).query;
 	var selectedId = queryData.id;
 	console.log('selectedId : '+selectedId);
-    res.render('feature');
+	var cardata = JSON.parse(fs.readFileSync('./data/cars.json', 'utf8'));
+	
+	var result = jsonQuery('cars[*id='+selectedId +']', {data: cardata}).value
+	console.log(result);
+    res.render('feature',result[0]);
 });
 
 
